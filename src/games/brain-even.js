732372dc36429ebce620dc8d06231d5e5e96ea70
Ivan Opara <oiv84@icloud.com>
2021-07-16 16:12:15 +0300

    refactor brain-even

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -6,20 +6,16 @@ const game = () => {
   // Game rules message.
   const gameRulesMessage = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-  const getCorrectAnswer = (num) => (num % 2 === 0 ? 'yes' : 'no');
-
   // Game settings.
-  const MAX_ATTEMPTS = 3;
+  let maxRounds = 3;
 
   // starting the game
   const name = greeting();
   console.log(gameRulesMessage);
 
-  let successfulAttempts = 0;
-
-  while (successfulAttempts < MAX_ATTEMPTS) {
+  while (maxRounds > 0) {
     const currentNumber = randomInt(0, 100);
-    const currentCorrectAnswer = getCorrectAnswer(currentNumber);
+    const currentCorrectAnswer = currentNumber % 2 === 0 ? 'yes' : 'no';
 
     console.log(`Question: ${currentNumber}`);
     const answer = readlineSync.question('Your answer: ');
@@ -32,9 +28,9 @@ const game = () => {
       console.log('Correct!');
     }
 
-    successfulAttempts += 1;
+    maxRounds -= 1;
 
-    if (successfulAttempts === MAX_ATTEMPTS) {
+    if (maxRounds === 0) {
       console.log(`Congratulations, ${name}!`);
     }
   }
