8d92b8af5ec5f2ac8aa43595dd8782ee988dde0e
Ivan Opara <oiv84@icloud.com>
2021-07-22 20:18:53 +0300

    some refactoring

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,15 +4,15 @@ import playGame from '../index.js';
 const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const numbersRange = [0, 100];
 
-const genRound = () => {
-  const isPrime = (num) => {
-    if (num < 2) return false;
-    for (let i = 2; i <= num / 2; i += 1) {
-      if (num % i === 0) return false;
-    }
-    return true;
-  };
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
 
+const genRound = () => {
   const question = randomInt(numbersRange);
   const correctAnswer = isPrime(question) ? 'yes' : 'no';
   return [question, correctAnswer];
